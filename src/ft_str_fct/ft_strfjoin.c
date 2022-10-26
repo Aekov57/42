1000479cc2389400d085efc6c44953998ad967d6
Aekov <97439098+Aekov57@users.noreply.github.com>
2022-10-26 16:00:48 +0200

    Update ft_strfjoin.c

--- src/ft_str_fct/ft_strfjoin.c
+++ src/ft_str_fct/ft_strfjoin.c
@@ -19,7 +19,7 @@ char	*ft_strfjoin(char *s1, char *s2)
 
 	if (!s1 || !s2)
 		return (NULL);
-	str = malloc(sizeof(char) * (ft_strlen(s1) + 2));
+	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!str)
 		return (NULL);
 	i = -1;
